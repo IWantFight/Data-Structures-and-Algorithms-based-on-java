8b2c49b2424d27aedc77db31091f04a94f23b1cf
ztw <1624042323@qq.com>
2020-02-12 22:40:06 +0800

    数据结构与算法

---
+++ src/P5/_1.java
@@ -0,0 +1,9 @@
+package P5;
+
+public class _1 {
+    public static void main(String[] args) {
+        String str = "java,java,hello";
+        String newstr = str.replaceAll("java", "尚硅谷");
+        System.out.println("newstr="+newstr);
+    }
+}
